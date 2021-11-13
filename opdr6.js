a74a2a39fceb5383199bc9f57e704dcbf70c4b09
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-13 12:44:09 +0100

    Add files via upload

---
+++ opdr6.js
@@ -0,0 +1,23 @@
+const paint = function (wall, color) {
+    console.log(wall + "has been painted" + color);
+};
+
+const wall = ["north wall ", "south wall ", "west wall ", "east wall "]
+/*const north = " north wall "
+const south = " south wall "
+const east = " east wall "
+const west = " west wall "
+*/
+
+//paint(" red ");
+
+const color = [" red ", " blue ", " green "]
+
+/*
+const color1 = " red "
+const color2 = " blue "
+const color3 = " green "
+*/
+
+paint(wall[1], color[2]);
+
